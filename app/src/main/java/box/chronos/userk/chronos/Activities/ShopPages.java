70540ef9ca1d2726adf79208eb4cb6d4ea6f7ca5
userk <balestrieri.gepp@gmail.com>
2016-12-14 18:09:39 +0100

    - aligned

---
+++ app/src/main/java/box/chronos/userk/chronos/Activities/ShopPages.java
@@ -0,0 +1,19 @@
+package box.chronos.userk.chronos.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import box.chronos.userk.chronos.R;
+
+/**
+ * Created by userk on 11/12/16.
+ */
+
+public class ShopPages extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.temp);
+    }
+}
