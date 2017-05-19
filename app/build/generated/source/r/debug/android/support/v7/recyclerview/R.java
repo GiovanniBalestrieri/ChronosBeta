e6b920051d7d20da9eaed4d305e183e34c5b7c2e
UserK <balestrieri.gepp@gmail.com>
2017-05-19 02:58:35 +0200

    Maps layout, zoom and tilt
    Working on offer card:
    
        ° Optimizing layout
        ° Centered distance tv
        ° preparing layout v2
        ° Viral info injection
        ° Dynamic timeout layout
    
    - Fb integration
    - Google+ ok integration
    
        Ok version 1.1
    - Added latest pictures (static)
    - Added Final price and Where layout
    - Added maps
    - Fixed Fragment Stack
     - Fixed Calendar string results
    - optimized layout profile
    - Fixed log out nullpointer
    - Added business name, phone, and address. Making them  dynamic
    - Adding business address, phone and businessName to share
    
    - Fixed Center category image with picasso: after load resize issue
    - Added elevation on profile picture
    - Profile v1 layout
    - Added rest interface
    
    - navigation drawer
    - Category layout
    - Offer page data transfer
    - Optimizing cardview and image caching
    - Added CAtegory Adapter
    - Added Category class
    - added category Fragment gridView
    - Fixed connection issues
    - multiple pictures

--- app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -8,21 +8,21 @@ package android.support.v7.recyclerview;
 
 public final class R {
     public static final class attr {
-        public static final int layoutManager = 0x7f01018f;
-        public static final int reverseLayout = 0x7f010191;
-        public static final int spanCount = 0x7f010190;
-        public static final int stackFromEnd = 0x7f010192;
+        public static final int layoutManager = 0x7f0101b8;
+        public static final int reverseLayout = 0x7f0101ba;
+        public static final int spanCount = 0x7f0101b9;
+        public static final int stackFromEnd = 0x7f0101bb;
     }
     public static final class dimen {
-        public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f0900f9;
-        public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f0900fa;
-        public static final int item_touch_helper_swipe_escape_velocity = 0x7f0900fb;
+        public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f0a011a;
+        public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f0a011b;
+        public static final int item_touch_helper_swipe_escape_velocity = 0x7f0a011c;
     }
     public static final class id {
-        public static final int item_touch_helper_previous_elevation = 0x7f0f0007;
+        public static final int item_touch_helper_previous_elevation = 0x7f100013;
     }
     public static final class styleable {
-        public static final int[] RecyclerView = { 0x010100c4, 0x010100f1, 0x7f01018f, 0x7f010190, 0x7f010191, 0x7f010192 };
+        public static final int[] RecyclerView = { 0x010100c4, 0x010100f1, 0x7f0101b8, 0x7f0101b9, 0x7f0101ba, 0x7f0101bb };
         public static final int RecyclerView_android_descendantFocusability = 1;
         public static final int RecyclerView_android_orientation = 0;
         public static final int RecyclerView_layoutManager = 2;
--- app/src/main/java/box/chronos/userk/chronos/activities/MapOffer.java
+++ app/src/main/java/box/chronos/userk/chronos/activities/MapOffer.java
@@ -1,8 +1,10 @@
 package box.chronos.userk.chronos.activities;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -11,7 +13,9 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import box.chronos.userk.chronos.R;
@@ -25,6 +29,8 @@ public class MapOffer extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap googleMap;
     LatLng shop;
     String mess;
+    private static final LatLng SYDNEY = new LatLng(-33.88,151.21);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,12 +73,53 @@ public class MapOffer extends AppCompatActivity implements OnMapReadyCallback {
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_aubergine_maps));
+
+            if (!success) {
+                Log.e("Maps", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("Nap", "Can't find style. Error: ", e);
+        }
+
+
         // Add a marker in Sydney, Australia,
         // and move the map's camera to the same location.
         CameraUpdate location = CameraUpdateFactory.newLatLngZoom(
                 shop, 17);
+
+        //googleMap.animateCamera(location);
+
+
+
+        // Move the camera instantly to Sydney with a zoom of 15.
+        //googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SYDNEY, 15));
+
+// Zoom in, animating the camera.
+        googleMap.animateCamera(CameraUpdateFactory.zoomIn());
+
+// Zoom out to zoom level 10, animating with a duration of 2 seconds.
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
+// Construct a CameraPosition focusing on Mountain View and animate the camera to that position.
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(shop)      // Sets the center of the map to Mountain View
+                .zoom(17)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(60)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+
         googleMap.addMarker(new MarkerOptions().position(shop).title(mess));
-        googleMap.animateCamera(location);
+
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+
     }
 
     @Override
