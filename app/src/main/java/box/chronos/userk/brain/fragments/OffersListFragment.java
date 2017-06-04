daa717c6b7f99f2d82babbde911b7ada8f389f95
UserK <balestrieri.gepp@gmail.com>
2017-06-05 00:07:27 +0200

    - Sort by distance _ Closest first - Adding articles - Added Share Intent in offer Page
    TODO
    solve Fb login on Android 6.0 +
    
    - Updated Code layout
    - Solved on UpNavigation btn press behave like a back button
    - Solved Facebook and GPlus Login
    - Added Triple tap social actions trigger
    - Optimizing Facebook login permissions
    - Fixed on register forced exit
    - Changed User Name  in Email
    - Fixed empty discount value
    - Fixed format float error and intorno a me
    - Refractor new package name
    - Shop status dynamic
    - Adding Phone call and opening Insegna dynamic
    - Updated version code
    - Fixed layout Shop info -> next add connection to shop account
    
    Todo:
    investigate spentmorethan10sec
    
    Working on offer card:
    
        ° Optimizing layout
        ° Centered distance tv
        ° preparing layout v2
        ° Viral info injection
        ° Dynamic timeout layout
    
    - Fixed Floating point comma issue
    - Glide vs Picasso
    - Offers' distance unit fix
    - Fixed map permission issue
    - Maps layout, zoom and tilt
    
    - Fb integration
    - Google+ ok integration
    
        Ok version 1.1
    - Added latest pictures (static)
    - Added Final price and Where layout
    - Added maps
    - Fixed Fragment Stack
     - Fixed Calendar string results
    - optimized layout profile
    - Fixed log out nullpointer
    - Added business name, phone, and address. Making them  dynamic
    - Adding business address, phone and businessName to share
    
    - Fixed Center category image with picasso: after load resize issue
    - Added elevation on profile picture
    - Profile v1 layout
    - Added rest interface
    
    - navigation drawer
    - Category layout
    - Offer page data transfer
    - Optimizing cardview and image caching
    - Added CAtegory Adapter
    - Added Category class
    - added category Fragment gridView
    - Fixed connection issues
    - multiple pictures

--- app/src/main/java/box/chronos/userk/brain/fragments/OffersListFragment.java
+++ app/src/main/java/box/chronos/userk/brain/fragments/OffersListFragment.java
@@ -6,12 +6,17 @@ import android.content.Intent;
 import android.graphics.Rect;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -82,6 +87,7 @@ public class OffersListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
 
     }
 
@@ -306,6 +312,41 @@ public class OffersListFragment extends Fragment {
         interaction.makeServiceRequest(AppUrls.COMMON_URL, pairs, TAG, "Dialog");
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_sort_distance_asc:
+                Toast.makeText(this.getActivity(),"Distanza crescente",Toast.LENGTH_SHORT);
+                Log.d("OffersList","Sort Distance Asc");
+                sortOffersDistanceAsc();
+                // Do Activity menu item stuff here
+                return true;
+            case R.id.action_sort_distance_desc:
+                Toast.makeText(this.getActivity(),"Distanza decrescente",Toast.LENGTH_SHORT);
+                Log.d("OffersList","Sort Distance Desc");
+                sortOffersDistanceDesc();
+                // Not implemented here
+                return false;
+
+            case R.id.action_sort_price_asc:
+                Toast.makeText(this.getActivity(),"Prezzo crescente",Toast.LENGTH_SHORT);
+                Log.d("OffersList","Sort Price Asc");
+                sortOffersPriceAsc();
+                // Do Activity menu item stuff here
+                return true;
+            case R.id.action_sort_price_desc:
+                Toast.makeText(this.getActivity(),"Prezzo decrescente",Toast.LENGTH_SHORT);
+                Log.d("OffersList","Sort Price Desc");
+                sortOffersPriceDesc();
+                // Not implemented here
+                return false;
+            default:
+                break;
+        }
+
+        return false;
+    }
+
     // get json data
     private void getJsonData(JSONObject object) {
         try {
@@ -357,4 +398,64 @@ public class OffersListFragment extends Fragment {
             e.printStackTrace();
         }
     }
+
+    private void sortOffersDistanceAsc() {
+        if (offerList.size() > 0) {
+            Collections.sort(offerList, new Comparator<Offer>() {
+                @Override
+                public int compare(Offer o1, Offer o2) {
+                    return Float.valueOf(o1.getDistance()).compareTo(Float.valueOf(o2.getDistance()));
+                }
+            });
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    private void sortOffersDistanceDesc() {
+        if (offerList.size() > 0) {
+            Collections.sort(offerList, new Comparator<Offer>() {
+                @Override
+                public int compare(Offer o1, Offer o2) {
+                    return Float.valueOf(o2.getDistance()).compareTo(Float.valueOf(o1.getDistance()));
+                }
+            });
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+
+    private void sortOffersPriceDesc() {
+        if (offerList.size() > 0) {
+            Collections.sort(offerList, new Comparator<Offer>() {
+                @Override
+                public int compare(Offer o1, Offer o2) {
+                    return Float.valueOf(o2.getPrice()).compareTo(Float.valueOf(o1.getPrice()));
+                }
+            });
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+
+    private void sortOffersPriceAsc() {
+        if (offerList.size() > 0) {
+            Collections.sort(offerList, new Comparator<Offer>() {
+                @Override
+                public int compare(Offer o1, Offer o2) {
+                    return Float.valueOf(o1.getPrice()).compareTo(Float.valueOf(o2.getPrice()));
+                }
+            });
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        // Inflate menu resource file.
+        inflater.inflate(R.menu.offer_list_menu, menu);
+
+    }
+
 }
